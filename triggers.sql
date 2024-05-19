c1501cf1d7566443afac14e7b80a148a797cc33e
pablovitorr <pablovitorstds@gmail.com>
2024-05-19 00:35:28 -0300

    Atualizando triggers

--- triggers.sql
+++ triggers.sql
@@ -1,17 +1,17 @@
 -- adiciona estoque ------------------------------------------------------------
-CREATE OR REPLACE TRIGGER t_movimentacao_estoque_compra
+CREATE OR REPLACE TRIGGER tg_estoque_movimentacao_compra
 AFTER INSERT OR UPDATE OR DELETE ON public.produtonotafiscalcompra
 FOR EACH ROW 
 EXECUTE FUNCTION public.movimentacao_estoque_compra();
 
 -- remove estoque -------------------------------------------------------------
-CREATE OR REPLACE TRIGGER t_movimentacao_estoque_venda
+CREATE OR REPLACE TRIGGER tg_estoque_movimentacao_venda
 AFTER INSERT OR UPDATE OR DELETE ON public.produtovenda
 FOR EACH ROW 
 EXECUTE FUNCTION public.movimentacao_estoque_venda();
 
 -- calculo de comissao --------------------------------------------------------
-CREATE OR REPLACE TRIGGER t_calculo_comissao_venda
+CREATE OR REPLACE TRIGGER tg_comissao_venda
 AFTER INSERT OR UPDATE ON public.venda 
 FOR EACH ROW 
 EXECUTE FUNCTION public.calculo_comissao_venda();
